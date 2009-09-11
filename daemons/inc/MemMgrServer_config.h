7b83cff6f6e1cb63681103ecae506c52fe9f8a02
Suman Anna <s-anna@ti.com>
2009-09-11 12:04:17 -0500

    SysLink: Daemons - Rename MemMgr header file
    The MemAllocServer_config.h file has been renamed to the
    correct file MemMgrServer_config.h. Also the license
    headers have been corrected for these header files.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: C A Subramaniam <subramaniam.ca@ti.com>

--- daemons/inc/MemMgrServer_config.h
+++ daemons/inc/MemMgrServer_config.h
@@ -1,53 +1,26 @@
+/*
+ * Syslink-IPC for TI OMAP Processors
+ *
+ * Copyright (C) 2009 Texas Instruments, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published
+ * by the Free Software Foundation version 2.1 of the License.
+ *
+ * This program is distributed .as is. WITHOUT ANY WARRANTY of any kind,
+ * whether express or implied; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
 /** ============================================================================
- *  @file   MemAllocServer_config.h
+ *  @file   MemMgrServer_config.h
  *
- *  @brief  MemAlloc Server configuration file
- *
- *  @ver    2.00.00.08
- *
- *  ============================================================================
- *
- *  Copyright (c) 2008-2009, Texas Instruments Incorporated
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *  *  Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *  *  Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *  *  Neither the name of Texas Instruments Incorporated nor the names of
- *     its contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- *  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- *  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
- *  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *  Contact information for paper mail:
- *  Texas Instruments
- *  Post Office Box 655303
- *  Dallas, Texas 75265
- *  Contact information:
- *  http://www-k.ext.ti.com/sc/technical-support/product-information-centers.htm?
- *  DCMP=TIHomeTracking&HQS=Other+OT+home_d_contact
+ *  @brief  MemMgr Server configuration file
  *  ============================================================================
  */
 
-#ifndef _MEMALLOCSERVER_CONFIG_H_
-#define _MEMALLOCSERVER_CONFIG_H_
+#ifndef _MEMMGRSERVER_CONFIG_H_
+#define _MEMMGRSERVER_CONFIG_H_
 
 #if defined (__cplusplus)
 extern "C" {
@@ -122,4 +95,4 @@ extern "C" {
 }
 #endif /* defined (__cplusplus) */
 
-#endif /* _MEMALLOCSERVER_CONFIG_H_ */
+#endif /* _MEMMGRSERVER_CONFIG_H_ */
