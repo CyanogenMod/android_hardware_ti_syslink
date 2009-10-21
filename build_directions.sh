ea386f92828a835c5599a16ccb2cf4e1f02aa492
Imtiaz Ahmed <x0116819@linuxomap.(none)>
2009-10-21 19:45:57 +0530

    SysLink: Build - Option in build_directions added to build Both Syslink & Bridge.
    Following are the changes in this patch.
    1. README updated to show the option to build both Syslink and Bridge.
    2. configure.ac of both bridge and syslink are modified to have DEBUG disabled\
       by default.
    3. Makefile.am in Daemon/syslink folder modified to change the name of the daemon\
       to syslink_daemons.out instead of syslink-daemon.out.
    4. proc_sample Makefile modified to include the rule to build all instead of default.
    5. samples folder Makefile modified to include the proc_samples subdirectory.
    
    Signed-off-by: Imtiaz Ahmed <x0116819@ti.com>

--- build_directions.sh
+++ build_directions.sh
@@ -13,6 +13,11 @@ echo "Enter path to tiler-userspace (currently '$TILER_USERSPACE'):\c"
 read VALUE
 export TILER_USERSPACE=${VALUE:=$TILER_USERSPACE}
 
+# path to kernel-syslink git root
+echo "Enter path to kernel-syslink (currently '$KRNLSRC'):\c"
+read VALUE
+export KRNLSRC=${VALUE:=$KRNLSRC}
+
 # path to userspace-space git root
 export USERSPACE_SYSLINK=`readlink -f $DIR`
 
@@ -24,6 +29,7 @@ echo TOOLBIN           is ${TOOLBIN}
 echo PREFIX            is ${PREFIX}
 echo TILER_USERSPACE   is ${TILER_USERSPACE}
 echo USERSPACE_SYSLINK is ${USERSPACE_SYSLINK}
+echo KRNLSRC	       is ${KRNLSRC}
 
 export PATH=${TOOLBIN}:$PATH
 
@@ -55,90 +61,99 @@ echo Found libpthread.so in $LIBPTHREAD
 #... Uncomment below if you want to enable DEBUG option.
 # ENABLE_DEBUG=--enable-debug
 
+build_syslink()
+{
+	# Building memmgr
+	echo "							   "
+	echo "*****************************************************"
+	echo "        Building tiler memmgr APIs and Samples	   "
+	echo "*****************************************************"
+	echo "							   "
+	cd ${TILER_USERSPACE}/memmgr
+	./bootstrap.sh
+	./configure --prefix ${PREFIX}/target --bindir ${PREFIX}/target/syslink \
+	--host ${HOST} --build i686-pc-linux-gnu
+	make clean > /dev/null 2>&1
+	make
+	if [[ $? -ne 0 ]] ; then
+	    exit 1
+	fi
+	make install
+	if [[ $? -ne 0 ]] ; then
+	    exit 1
+	fi
+	# Building syslink
+	#.. need libgcc.a, librt.so and libpthread.so
+	mkdir -p ${PREFIX}/target/lib
+	cp $LIBGCC ${PREFIX}/target/lib
+	cp `dirname $LIBRT`/librt*.so* ${PREFIX}/target/lib
+	cp `dirname $LIBPTHREAD`/libpthread*.so* ${PREFIX}/target/lib
+	#.. syslink prefix needs a target subdirectory,
+	#so we will create link to the parent
+	cd ${USERSPACE_SYSLINK}/syslink
+	echo "							  "
+	echo "****************************************************"
+	echo "      Building Syslink APIs and Samples		  "
+	echo "****************************************************"
+	echo "							  "
+	./autogen.sh
+	./configure --prefix ${PREFIX}/target --bindir ${PREFIX}/target/syslink \
+	--host ${HOST} ${ENABLE_DEBUG}  --build i686-pc-linux-gnu
+	export TILER_INC_PATH=${TILER_USERSPACE}/memmgr
+	make clean > /dev/null 2>&1
+	make
+	if [[ $? -ne 0 ]] ; then
+	    exit 1
+	fi
+	make install
+	if [[ $? -ne 0 ]] ; then
+	    exit 1
+	fi
+}
+
+build_bridge()
+{
+	# Building tesla bridge
+	echo "							  "
+	echo "****************************************************"
+	echo "	    Building Bridge APIs and Samples		  "
+	echo "****************************************************"
+	echo "							  "
+	cd ${USERSPACE_SYSLINK}/bridge
+	./autogen.sh
+	./configure --prefix ${PREFIX}/target --bindir ${PREFIX}/target/dspbridge \
+	--host ${HOST} ${ENABLE_DEBUG}  --build i686-pc-linux-gnu
+	make clean > /dev/null 2>&1
+	make
+	if [[ $? -ne 0 ]] ; then
+	    exit 1
+	fi
+	make install
+	if [[ $? -ne 0 ]] ; then
+	    exit 1
+	fi
+	cd -
+}
+
+
 echo "	"
 echo "Following are the 2 Build options available:"
 echo "--------------------------------------------"
 echo "1--------------> Build Syslink Only"
 echo "2--------------> Build Bridge Only"
+echo "3--------------> Build Syslink & Bridge"
 echo "Any other Option to exit from Build system"
 echo "	"
 echo "Enter your option:"
 read VALUE
 case $VALUE in
         1)
-		# Building memmgr
-		echo "							   "
-		echo "*****************************************************"
-		echo "        Building tiler memmgr APIs and Samples	   "
-		echo "*****************************************************"
-		echo "							   "
-		cd ${TILER_USERSPACE}/memmgr
-		./bootstrap.sh
-		./configure --prefix ${PREFIX}/target --bindir ${PREFIX}/target/syslink \
-		--host ${HOST} --build i686-pc-linux-gnu
-		make clean > /dev/null 2>&1
-		make
-		if [[ $? -ne 0 ]] ; then
-		    exit 1
-		fi
-		make install
-		if [[ $? -ne 0 ]] ; then
-		    exit 1
-		fi
-		# Building syslink
-		#.. need libgcc.a, librt.so and libpthread.so
-		mkdir -p ${PREFIX}/target/lib
-		cp $LIBGCC ${PREFIX}/target/lib
-		cp `dirname $LIBRT`/librt*.so* ${PREFIX}/target/lib
-		cp `dirname $LIBPTHREAD`/libpthread*.so* ${PREFIX}/target/lib
-		#.. syslink prefix needs a target subdirectory,
-		#so we will create link to the parent
-		cd ${USERSPACE_SYSLINK}/syslink
-		echo "							  "
-		echo "****************************************************"
-		echo "      Building Syslink APIs and Samples		  "
-		echo "****************************************************"
-		echo "							  "
-		./autogen.sh
-		./configure --prefix ${PREFIX}/target --bindir ${PREFIX}/target/syslink \
-		--host ${HOST} ${ENABLE_DEBUG}  --build i686-pc-linux-gnu
-		export TILER_INC_PATH=${TILER_USERSPACE}/memmgr
-		make clean > /dev/null 2>&1
-		make
-		if [[ $? -ne 0 ]] ; then
-		    exit 1
-		fi
-		make install
-		if [[ $? -ne 0 ]] ; then
-		    exit 1
-		fi
-
-		;; # End of case 1
-
+		build_syslink ;; # End of case 1
 	2)
-
-		# Building tesla bridge
-		echo "	  						  "
-		echo "****************************************************"
-		echo "	    Building Bridge APIs and Samples		  "
-		echo "****************************************************"
-		echo "							  "
-		cd ${USERSPACE_SYSLINK}/bridge
-		./autogen.sh
-		./configure --prefix ${PREFIX}/target --bindir ${PREFIX}/target/dspbridge \
-		--host ${HOST} ${ENABLE_DEBUG}  --build i686-pc-linux-gnu
-		make clean > /dev/null 2>&1
-		make
-		if [[ $? -ne 0 ]] ; then
-		    exit 1
-		fi
-		make install
-		if [[ $? -ne 0 ]] ; then
-		    exit 1
-		fi
-		cd -
-		;; # End of case 2
-
+		build_bridge ;; # End of case 2
+	3)
+		build_syslink
+		build_bridge ;; # End of case 3
 	*)	echo " Exiting from the build system....... "
 		exit 1
 		;;
