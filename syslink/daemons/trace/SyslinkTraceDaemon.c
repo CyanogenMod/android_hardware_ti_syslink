5adc7bedadb5d978f6109f1e9d4dad597b18f632
Suman Anna <s-anna@ti.com>
2009-11-11 05:13:30 -0600

    SysLink: Daemons - Syslink Trace daemon formatted for Syslink coding style
    Syslink Trace daemon has been formatted to follow the Syslink
    coding style.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- syslink/daemons/trace/SyslinkTraceDaemon.c
+++ syslink/daemons/trace/SyslinkTraceDaemon.c
@@ -51,33 +51,33 @@ extern "C" {
 
 
 // pull char from queue
-void printSysM3Traces(void *arg)
+Void printSysM3Traces (Void *arg)
 {
-
-    Int                             status = 0;
-    Memory_MapInfo                  traceinfo;
-    UInt32                          numOfBytesInBuffer = 0;
-    UInt32  *readPointer;
-    UInt32  *writePointer;
-    char    *traceBuffer;
+    Int             status              = 0;
+    Memory_MapInfo  traceinfo;
+    UInt32          numOfBytesInBuffer  = 0;
+    UInt32        * readPointer;
+    UInt32        * writePointer;
+    Char          * traceBuffer;
 
     UsrUtilsDrv_setup ();
+
+    Osal_printf ("\nSpawning SysM3 trace thread\n ");
+
     /* Get the user virtual address of the buffer */
     traceinfo.src  = SYSM3_TRACE_BUFFER_PHYS_ADDR;
     traceinfo.size = TRACE_BUFFER_SIZE;
-    Osal_printf("\nSpawning SysM3 trace thread\n ");
     status = Memory_map (&traceinfo);
     readPointer = (UInt32 *)traceinfo.dst;
     writePointer = (UInt32 *)(traceinfo.dst + 0x4);
-    traceBuffer = (char *)(traceinfo.dst + 0x8);
+    traceBuffer = (Char *)(traceinfo.dst + 0x8);
 
     /* Initialze read and write indexes to zero */
     *readPointer = 0;
     *writePointer = 0;
     do {
-
         do {
-           sleep(TIMEOUT_SECS);
+           sleep (TIMEOUT_SECS);
         } while (*readPointer == *writePointer);
 
         if ( *readPointer < *writePointer ) {
@@ -85,53 +85,56 @@ void printSysM3Traces(void *arg)
         } else {
             numOfBytesInBuffer = (TRACE_BUFFER_SIZE - *readPointer) + *writePointer;
         }
-        Osal_printf("\n[SYSM3]: ");
-        while ( numOfBytesInBuffer-- )
-        {
-            printf("%c", traceBuffer[*readPointer]);
-            if (traceBuffer[*readPointer] == '\n')
-                Osal_printf("[SYSM3]: ");
 
-            if (*readPointer == (TRACE_BUFFER_SIZE - 1))
+        Osal_printf ("\n[SYSM3]: ");
+        while ( numOfBytesInBuffer-- ) {
+            Osal_printf ("%c", traceBuffer[*readPointer]);
+            if (traceBuffer[*readPointer] == '\n') {
+                Osal_printf ("[SYSM3]: ");
+            }
+
+            if (*readPointer == (TRACE_BUFFER_SIZE - 1)) {
                 *readPointer = 0;
-            else
+            }
+            else {
                 (*readPointer)++;
+            }
         }
-
     } while(1);
+
     Osal_printf ("Leaving printSysM3Traces thread function \n");
     return;
 }
 
 
 // pull char from queue
-void printAppM3Traces(void *arg)
+Void printAppM3Traces (Void *arg)
 {
-
-    Int                             status = 0;
-    Memory_MapInfo                  traceinfo;
-    UInt32                          numOfBytesInBuffer = 0;
-    UInt32  *readPointer;
-    UInt32  *writePointer;
-    char    *traceBuffer;
+    Int             status              = 0;
+    Memory_MapInfo  traceinfo;
+    UInt32          numOfBytesInBuffer  = 0;
+    UInt32        * readPointer;
+    UInt32        * writePointer;
+    Char          * traceBuffer;
 
     UsrUtilsDrv_setup ();
+
+    Osal_printf ("\nSpawning APP-M3 trace thread\n ");
+
     /* Get the user virtual address of the buffer */
     traceinfo.src  = APPM3_TRACE_BUFFER_PHYS_ADDR;
     traceinfo.size = TRACE_BUFFER_SIZE;
-    Osal_printf("\nSpawning APP-M3 trace thread\n ");
     status = Memory_map (&traceinfo);
     readPointer = (UInt32 *)traceinfo.dst;
     writePointer = (UInt32 *)(traceinfo.dst + 0x4);
-    traceBuffer = (char *)(traceinfo.dst + 0x8);
+    traceBuffer = (Char *)(traceinfo.dst + 0x8);
 
     /* Initialze read and write indexes to zero */
     *readPointer = 0;
     *writePointer = 0;
     do {
-
         do {
-           sleep(TIMEOUT_SECS);
+           sleep (TIMEOUT_SECS);
         } while (*readPointer == *writePointer);
 
         if ( *readPointer < *writePointer ) {
@@ -139,20 +142,23 @@ void printAppM3Traces(void *arg)
         } else {
             numOfBytesInBuffer = (TRACE_BUFFER_SIZE - *readPointer) + *writePointer;
         }
-        Osal_printf("\n[APPM3]: ");
-        while ( numOfBytesInBuffer-- )
-        {
-            printf("%c", traceBuffer[*readPointer]);
-            if (traceBuffer[*readPointer] == '\n')
-                Osal_printf("[APPM3]: ");
 
-            if (*readPointer == (TRACE_BUFFER_SIZE - 1))
+        Osal_printf ("\n[APPM3]: ");
+        while ( numOfBytesInBuffer-- ) {
+            Osal_printf ("%c", traceBuffer[*readPointer]);
+            if (traceBuffer[*readPointer] == '\n') {
+                Osal_printf ("[APPM3]: ");
+            }
+
+            if (*readPointer == (TRACE_BUFFER_SIZE - 1)) {
                 *readPointer = 0;
-            else
+            }
+            else {
                 (*readPointer)++;
+            }
         }
-
     } while(1);
+
     Osal_printf ("Leaving printAppM3Traces thread function \n");
     return;
 }
@@ -160,47 +166,51 @@ void printAppM3Traces(void *arg)
 
 Int main (Int argc, Char * argv [])
 {
-    pid_t child_pid, child_sid;
-    Int status;
-    pthread_t                       thread_sys; /* server thread object */
-    pthread_t                       thread_app; /* server thread object */
+    pid_t       child_pid;
+    pid_t       child_sid;
+    pthread_t   thread_sys; /* server thread object */
+    pthread_t   thread_app; /* server thread object */
 
-    Osal_printf("Spawning Ducati Trace daemon...\n");
+    Osal_printf ("Spawning Ducati Trace daemon...\n");
 
     /* Fork off the parent process */
-    child_pid = fork();
+    child_pid = fork ();
     if (child_pid < 0) {
-        Osal_printf("Spawn daemon failed!\n");
-        exit(EXIT_FAILURE);     /* Failure */
+        Osal_printf ("Spawning Trace daemon failed!\n");
+        exit (EXIT_FAILURE);     /* Failure */
     }
+
     /* If we got a good PID, then we can exit the parent process. */
     if (child_pid > 0) {
-        exit(EXIT_SUCCESS);    /* Succeess */
+        exit (EXIT_SUCCESS);    /* Succeess */
     }
 
     /* Change file mode mask */
-    umask(0);
+    umask (0);
 
     /* Create a new SID for the child process */
-    child_sid = setsid();
+    child_sid = setsid ();
     if (child_sid < 0)
     {
-        Osal_printf("setsid failed!\n");
-        exit(EXIT_FAILURE);     /* Failure */
+        Osal_printf ("setsid failed!\n");
+        exit (EXIT_FAILURE);     /* Failure */
     }
+
     /* Change the current working directory */
     if ((chdir("/")) < 0) {
-        Osal_printf("chdir failed!\n");
-        exit(EXIT_FAILURE);     /* Failure */
+        Osal_printf ("chdir failed!\n");
+        exit (EXIT_FAILURE);     /* Failure */
     }
+
     pthread_create (&thread_sys, NULL, (Void *)&printSysM3Traces,
                     NULL);
     pthread_create (&thread_app, NULL, (Void *)&printAppM3Traces,
                     NULL);
-    pthread_join(thread_sys, NULL);
-    Osal_printf("sysm3 trace thread exited\n");
-    pthread_join(thread_app, NULL);
-    Osal_printf("appm3 trace thread exited\n");
+
+    pthread_join (thread_sys, NULL);
+    Osal_printf ("SysM3 trace thread exited\n");
+    pthread_join (thread_app, NULL);
+    Osal_printf ("AppM3 trace thread exited\n");
 
     return 0;
 }
