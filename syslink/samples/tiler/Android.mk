c97c2bff5065effa35c8dab6bb8e0e7c4607d6cb
Shivananda Hebbar <x0hebbar@ti.com>
2010-06-24 10:23:54 -0500

    SysLink: Build - Remove SysMgr dependencies from Tiler samples
    The Tiler sample files have been updated to remove the dependency of
    linking against the SysMemMgr library. The compiler macro
    SYSLINK_USE_SYSMGR has also been removed from these samples. There is
    no non-SysMgr variant concept with the latest SysLink code.
    
    Signed-off-by: Shivananda Hebbar <x0hebbar@ti.com>
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- syslink/samples/tiler/Android.mk
+++ syslink/samples/tiler/Android.mk
@@ -12,6 +12,7 @@ TilerTestApp.c
 
 LOCAL_C_INCLUDES += \
 	$(LOCAL_PATH)/../../api/include \
+	$(LOCAL_PATH)/../../api/include/ti/ipc \
 	$(LOCAL_PATH)/../inc \
 	hardware/ti/tiler/memmgr
 
@@ -21,11 +22,10 @@ LOCAL_SHARED_LIBRARIES := \
 	librcm \
 	libnotify \
 	libsysmgr \
-	libsysmemmgr \
 	libmemmgr
 
 
-LOCAL_CFLAGS += -MD -pipe  -fomit-frame-pointer -Wall  -Wno-trigraphs -Werror-implicit-function-declaration  -fno-strict-aliasing -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -msoft-float -Uarm -DMODULE -D__LINUX_ARM_ARCH__=7  -fno-common -DLINUX -DTMS32060 -D_DB_TIOMAP  -DSYSLINK_USE_SYSMGR -DSYSLINK_USE_LOADER
+LOCAL_CFLAGS += -MD -pipe  -fomit-frame-pointer -Wall  -Wno-trigraphs -Werror-implicit-function-declaration  -fno-strict-aliasing -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -msoft-float -Uarm -DMODULE -D__LINUX_ARM_ARCH__=7  -fno-common -DLINUX -DTMS32060 -D_DB_TIOMAP -DSYSLINK_USE_LOADER
 #LOCAL_CFLAGS += -DSYSLINK_USE_DAEMON
 
 LOCAL_MODULE:= syslink_tilertest.out
