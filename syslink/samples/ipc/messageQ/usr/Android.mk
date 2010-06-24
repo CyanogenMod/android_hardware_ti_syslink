1f9a9a73ef14655ca43b8e905313067a392ec2eb
Shivananda Hebbar <x0hebbar@ti.com>
2010-06-24 10:27:33 -0500

    SysLink: Build - Remove MessageQApp1 build support
    Cleaned up the Makefiles to remove building MessageQApp1 sample.
    
    Signed-off-by: Shivananda Hebbar <x0hebbar@ti.com>

--- syslink/samples/ipc/messageQ/usr/Android.mk
+++ syslink/samples/ipc/messageQ/usr/Android.mk
@@ -12,15 +12,3 @@ LOCAL_SHARED_LIBRARIES := libipcutils libipc librcm libnotify libsysmgr
 LOCAL_CFLAGS += -MD -pipe  -fomit-frame-pointer -Wall  -Wno-trigraphs -Werror-implicit-function-declaration  -fno-strict-aliasing -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -msoft-float -Uarm -DMODULE -D__LINUX_ARM_ARCH__=7  -fno-common -DLINUX -DTMS32060 -D_DB_TIOMAP -DSYSLINK_USE_LOADER
 LOCAL_MODULE:= messageQApp.out
 include $(BUILD_EXECUTABLE)
-
-include $(CLEAR_VARS)
-LOCAL_ARM_MODE := arm
-LOCAL_SRC_FILES:= MessageQAppOS1.c MessageQApp1.c
-LOCAL_C_INCLUDES += \
-	$(LOCAL_PATH)/../../../inc \
-	$(LOCAL_PATH) \
-	$(LOCAL_PATH)/../../../../api/include
-LOCAL_SHARED_LIBRARIES := libipcutils libipc libnotify libsysmgr
-LOCAL_CFLAGS += -MD -pipe  -fomit-frame-pointer -Wall  -Wno-trigraphs -Werror-implicit-function-declaration  -fno-strict-aliasing -mapcs -mno-sched-prolog -mabi=aapcs-linux -mno-thumb-interwork -msoft-float -Uarm -DMODULE -D__LINUX_ARM_ARCH__=7  -fno-common -DLINUX -DTMS32060 -D_DB_TIOMAP -DSYSLINK_USE_LOADER
-LOCAL_MODULE:= messageQApp1.out
-include $(BUILD_EXECUTABLE)
