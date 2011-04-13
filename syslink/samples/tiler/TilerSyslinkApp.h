b25cafb0e5d0990bb3e2a41552ec1beeb1058a85
Subramaniam C.A <subramaniam.ca@ti.com>
2011-04-13 13:21:59 -0500

    SysLink: Samples - Convert LGPL license to Apache license
    This patch changes the licensing of all sample files from
    LGPL v2.1 license to Apache v2.0 license. These files should
    have been created with Apache license like the rest of the
    sample source code originally.
    
    Signed-off-by: Subramaniam C.A <subramaniam.ca@ti.com>

--- syslink/samples/tiler/TilerSyslinkApp.h
+++ syslink/samples/tiler/TilerSyslinkApp.h
@@ -1,16 +1,17 @@
 /*
- * Syslink-IPC for TI OMAP Processors
+ *  Copyright 2008-2011 Texas Instruments - http://www.ti.com/
  *
- * Copyright (C) 2009 Texas Instruments, Inc.
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as published
- * by the Free Software Foundation version 2.1 of the License.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * This program is distributed .as is. WITHOUT ANY WARRANTY of any kind,
- * whether express or implied; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 /** ============================================================================
  *  @file   TilerSysLinkApp.h
